8d46b767ffe22061961f724e744a238434d400c6
Hyesooo <hyesookim95@gmail.com>
2023-05-31 08:05:14 +0900

    🪛[chore]: 스프링 프로젝트 초기 구축

---
+++ src/main/java/com/pingpong/iosapp/IosAppApplication.java
@@ -0,0 +1,13 @@
+package com.pingpong.iosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IosAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pingpong/iosapp/IosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingpong.iosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IosAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
