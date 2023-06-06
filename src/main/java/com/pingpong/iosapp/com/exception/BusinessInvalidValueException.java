4659e1c8644a3d7694a7e7666d2a1061382d8815
Hyesooo <hyesookim95@gmail.com>
2023-06-06 11:43:25 +0900

    ✨[feat]: global exception handler 구현

---
+++ src/main/java/com/pingpong/iosapp/com/exception/BusinessInvalidValueException.java
@@ -0,0 +1,8 @@
+package com.pingpong.iosapp.com.exception;
+
+public class BusinessInvalidValueException extends RuntimeException {
+
+    public BusinessInvalidValueException(String message) {
+        super(message);
+    }
+}
