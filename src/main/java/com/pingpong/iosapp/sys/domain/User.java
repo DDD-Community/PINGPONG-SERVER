221ff38d10cd4a76f55ce8dd008fe96963bb96c4
Hyesooo <hyesookim95@gmail.com>
2023-07-11 07:57:46 +0900

    ✨[feat]: 구글로그인 기능추가

--- src/main/java/com/pingpong/iosapp/sys/domain/User.java
+++ src/main/java/com/pingpong/iosapp/sys/domain/User.java
@@ -29,13 +29,26 @@ public class User implements UserDetails {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
 
+    // 사용자이름변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
+
+
+    // 권한 반환
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("user"));
@@ -53,21 +66,25 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
+        // todo: 만료되었는지 확인하는 로직
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+        // todo: 잠금되었는지 확인하는 로직
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+        // todo: 패스워드가 만료되었는지 확인하는 로직
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+        // todo: 계정이 사용가능한지 확인하는 로직
         return true;
     }
 }
--- src/main/java/com/pingpong/iosapp/sys/service/UserService.java
+++ src/main/java/com/pingpong/iosapp/sys/service/UserService.java
@@ -12,18 +12,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Long save(AddUserRequestDto userReqDto) {
+    public Long save(AddUserRequestDto dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
-                .email(userReqDto.getEmail())
-                .auth(userReqDto.getAuth())
-                .password(bCryptPasswordEncoder.encode(userReqDto.getPassword()))
+                .email(dto.getEmail())
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
     public User findById(Long userId) {
         return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 
