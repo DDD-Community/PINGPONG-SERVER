f3006f5e0e79d0b60faeb68a4062274be4dd1aa9
HyesooKim <hyesoo_kim2@kolon.com>
2023-07-27 16:47:13 +0900

    :sparkles:[feat]: 엔티티 추가

---
+++ src/main/java/com/pingpong/quoteBakery/app/domain/Like.java
@@ -0,0 +1,32 @@
+package com.pingpong.quoteBakery.app.domain;
+
+import com.pingpong.quoteBakery.com.entity.BaseEntity;
+import com.pingpong.quoteBakery.sys.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+/**
+ * 명언 좋아요 Entity
+ */
+@Entity
+@Table(name = "likes")
+public class Like extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @SequenceGenerator(name = "sequence_generator", sequenceName = "my_sequence", allocationSize = 50)
+    private Long like_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "quote_id")
+    private Quote quote;
+}
