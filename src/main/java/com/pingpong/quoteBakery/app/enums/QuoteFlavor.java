d041964352fc046333e93a56009590b9d70ffeba
HyesooKim <hyesoo_kim2@kolon.com>
2023-07-28 09:45:30 +0900

    :pencil2:[correct]: correct typos

--- src/main/java/com/pingpong/quoteBakery/app/enums/QuoteFlavor.java
+++ src/main/java/com/pingpong/quoteBakery/app/enums/QuoteFlavor.java
@@ -1,7 +1,4 @@
-/**
- * Copyright ⓒ Kolon Benit CO. LTD. All rights reserved.
- * 코오롱베니트(주)의 사전 승인 없이 본 내용의 전부 또는 일부에 대한 복사, 배포, 사용을 금합니다.
- */
+
 package com.pingpong.quoteBakery.app.enums;
 
 import lombok.Getter;
@@ -11,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum QuoteFlavor {
     NUTTY("nutty", "고소한맛"),
-    LIGTH("ligth", "담백한맛"),
+    LIGHT("light", "담백한맛"),
     SWEET("sweet", "달콤한맛"),
     SALTY("salty", "짭잘한맛"),
     SPICY("spicy", "매콤한맛");
--- src/main/java/com/pingpong/quoteBakery/app/enums/QuoteSource.java
+++ src/main/java/com/pingpong/quoteBakery/app/enums/QuoteSource.java
@@ -1,7 +1,3 @@
-/**
- * Copyright ⓒ Kolon Benit CO. LTD. All rights reserved.
- * 코오롱베니트(주)의 사전 승인 없이 본 내용의 전부 또는 일부에 대한 복사, 배포, 사용을 금합니다.
- */
 package com.pingpong.quoteBakery.app.enums;
 
 import lombok.Getter;
